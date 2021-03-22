2eb4b241c4b73dfcea199dc61bcf229343883be8
MahmoudMiehob <mahmoudmiehob@gmail.com>
2021-03-22 12:37:54 +0300

    Ecommerce Admin files layout

---
+++ Admin/header.php
@@ -0,0 +1,16 @@
+<?php include 'int.php';
+      include 'include/function/function.php';
+       ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php getTitle() ?></title>
+        <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <link rel="stylesheet" href= "<?php echo $css ; ?>bootstrap.min.css.map" >
+        <link rel="stylesheet" href= "<?php echo $css ; ?>bootstrap.min.css" >       
+        <link rel="stylesheet" href= "<?php echo $css ; ?>backendsss.css" >
+    </head>
+    <body>
+     
